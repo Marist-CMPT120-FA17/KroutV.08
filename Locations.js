8a44c465fa82eac53855a71be02e9264ef5b3f79
Ian Krout <ian.krout1@marist.edu>
2017-11-09 14:19:42 -0500

    Progress

--- Locations.js
+++ Locations.js
@@ -7,6 +7,8 @@
  var items = [];
  var Keytaken = 0;
  var Hoagietaken= 0;
+ var Maptaken= 0;
+ var Coattaken= 0;
  var currentLoc = 0;
 
  var VisitedLoc0 = 0;
@@ -72,8 +74,8 @@
 	var locationZero = new Locations (0, "Main Entrance", "1. You enter Donnelly Hall in a panic realizing your dog, Donnelly, has gotten loose and made his way to his favorite building. You must find him!", "map");
 	var locationOne = new Locations (1, "Security Office", "2. You are at security, they say they saw your dog but can't say where he is for sure since its a circular building...Security offers you a key to all the rooms (enter take to pick up key)", "key");
 	var locationTwo = new Locations (2, "Camera Room", "3. You rush to the back room to check the security cameras and see your dog running in circles", null );
-	var locationThree = new Locations (3, "Fashion Lab", "8.You are the fashion lab and see nothing but clothes.", "coat");
-	var locationFour = new Locations (4, "Outside Building", "You leave the building. Try again.", null);
+	var locationThree = new Locations (3, "Fashion Lab", "8.You are the fashion lab and see nothing but clothes. Need a new coat? enter take.", "coat");
+	var locationFour = new Locations (4, "Outside Building", "You leave the building. Try again.", null); 
 	var locationFive = new Locations (5, "Chemistry Lab", "4.You head down to the chemistry lab and hear barking", null);
 	var locationSix = new Locations (6, " Found Dog", "6. Donnelly! You found your dog, sitting by the bathroom.", null);
 	var locationSeven = new Locations (7, " DN Cafe", "9. You are at the DN Cafe, maybe Donnelly was attracted to the food?", null);
@@ -92,8 +94,10 @@ function Items (id, name, description) {
 }
 	var key = new Items (0,"key", "you have picked up a key");
 	var hoagie = new Items (1,"hoagie", "you have picked up a hoagie");
+	var map = new Items (2,"map","you have picked up a map");
+	var coat = new Items (3,"coat", "you have picked up a coat");
 
-	items.push (key, hoagie);
+	items.push (key, hoagie, map, coat);
 	console.log(items);
 	
  //Nested if to pick up Items
@@ -114,7 +118,24 @@ function TakeItem () {
 			Hoagietaken = 1;
 		}
 	}
+	if (currentLoc === 0) {
+		if (Maptaken == 0) {
+			inventory.push(map.name);
+			console.log(inventory);
+			UpdateInventory(map.description)
+			Maptaken = 1;
+		}
+	}
+	if (currentLoc === 3) {
+		if (Coattaken == 0) {
+			inventory.push(coat.name);
+			console.log(inventory);
+			UpdateInventory(coat.description)
+			Coattaken = 1;
+		}
+	}
 }
+
 			console.log(inventory);
 //Listing Inventory
  function listInventory () {
